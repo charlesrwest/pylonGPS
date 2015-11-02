ef872f7b66a179f80188d85ba9e04f78026f76ca
Charles West <crwest@ncsu.edu>
2015-11-02 15:46:42 -0500

    Setup build enviroment to create Qt5 Guis for use with the Transceiver.

---
+++ qml/main.qml
@@ -0,0 +1,15 @@
+import QtQuick 2.3
+
+Item 
+{
+id: root
+width: 400; height: 250
+
+Rectangle
+{
+id: rectangle
+border.color: "white"
+anchors.fill: parent
+}
+
+}
---
+++ src/executables/transceiverGUI/main.cpp
@@ -0,0 +1,23 @@
+#include<QApplication>
+#include<QQuickView>
+#include<QUrl>
+#include<memory>
+#include "SOMException.hpp"
+
+int main(int argc, char **argv)
+{
+QApplication app(argc, argv);
+std::unique_ptr<QQuickView> widget;
+
+SOM_TRY
+widget.reset(new QQuickView);
+SOM_CATCH("Error initializing widget\n")
+
+QUrl source = QUrl::fromLocalFile("qml/main.qml");
+
+widget->setSource(source);
+
+widget->show();
+
+return app.exec();
+} 
